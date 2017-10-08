19182577e60ad5c3d31bf664aae397f193fd02ac
ziggi24 <ziggi24@gmail.com>
2017-10-08 12:27:16 -0700

    added comments to script

--- wordCount.py
+++ wordCount.py
@@ -5,13 +5,28 @@
 #@since 2017-10-6
 #@modified 2017-10-6
 
-import re
-import string
+#for this project we will use the Counter Object, which is based on the python
+#dictionaries. this will automatically handle the parsing of words, as well as
+#counting their frequency. It will also provide the most_common() method that
+#we use to calculate the 100 most common and return them to the user. The
+# documentation for Counter objects can be found here:
+#
+#https://docs.python.org/3/library/collections.html
 from collections import Counter
 
+# here we ask the user which file to parse. For the sake of this example the
+#file will always be the included input.txt, however I added this input so that
+#other people could take this code and use it without having to modify it.
 fileName = input("what is the file name of the input text? ")
 
+# with the file name given, we will open the file, and read it.
+#This is the main loop of the script. This check to see if the file exists, then
+#opens it, and proceeds to loop through every word and adds it to the Counter
+#object, called count here. This will return completed when there are no more
+#words to parse.
 with open(fileName) as doc:
     count = Counter(doc.read().strip().split().lower())
 
+# this is the print line statement. Currently this is not formatted, and will
+# print out a messy array with the top 100 most used words. 
 print(count.most_common(100))
